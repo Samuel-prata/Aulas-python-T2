a6a14f816ede57905bc7e3924645d0821b5538e3
samuel-prata <samuel.silverio001@hotmail.com>
2025-02-26 18:09:49 -0300

    add: aula 02 - strings e estruturas de controle

---
+++ aula-02/repeticao.py
@@ -0,0 +1,11 @@
+''' loops -> FOR E WHILE'''
+
+
+for i in range(0, 21, 4):
+    print(f'Esse ano faço {i} anos!')
+
+numero = 0
+
+while numero <= 20:
+    print(numero)
+    numero +=1 # -> numero = numero +1
